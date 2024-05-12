6f4fafd78632d881a62f953b01027dc3361994fb
Raquel Berenguer <raquelberenguer@Raquels-MacBook-Pro.local>
2024-05-12 16:52:44 +0200

    =Comment country insert table for a future use

--- database/insert-data.sh
+++ database/insert-data.sh
@@ -99,8 +99,8 @@ rm /data/formatted/inproceedings/inproceedings_7.xml
 echo "start insert relationships and insert institutions"
 psql -U postgres -f database/importer-relationships-many-to-many.sql
 
-echo "Add countries in institutions"
-psql -U postgres -f database/add_country.sql
+#echo "Add countries in institutions"
+#psql -U postgres -f database/add_country.sql
 
 echo "start update publications add num_pages"
 psql -U postgres -f database/number-pages-publications.sql
